301381c0c102a645a2cce3665ba245b387bfdae1
Jacielio Queiroz <cielioqueiroz@hotmail.com>
2024-05-22 12:30:27 -0300

    refactoring: significant changes to the entire project structure

--- js/script.js
+++ js/script.js
@@ -1,11 +1,43 @@
 const searchText = document.getElementById("search");
 const qrContainer = document.getElementById("qr-container");
 const generateBtn = document.getElementById("generate-btn");
+const shareBtn = document.getElementById("share-btn");
 let lastInputValue = "";
 
 function clearInput() {
   searchText.value = "";
   qrContainer.innerHTML = "";
+}
+
+function showToast(message) {
+  Toastify({
+    text: message,
+    duration: 3000,
+    gravity: "top",
+    position: "center",
+    style: {
+      background: "#fff",
+      color: "#000",
+      borderBottom: "4px solid #336699",
+    },
+    stopOnFocus: true,
+  }).showToast();
+}
+
+function shareQRCode() {
+  if (!navigator.share) {
+    showToast("Compartilhamento não suportado neste navegador.");
+    return;
+  }
+
+  const qrCodeDataURL = qrContainer.querySelector("img").src;
+  navigator.share({
+    title: "QR Code",
+    text: "Veja este QR Code",
+    url: qrCodeDataURL,
+  })
+  .then(() => showToast("QR Code compartilhado com sucesso!"))
+  .catch(() => showToast("Erro ao compartilhar o QR Code."));
 }
 
 function qrCodeGenerator() {
@@ -14,15 +46,16 @@ function qrCodeGenerator() {
       qrContainer.innerHTML = `<img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${searchText.value}" alt="QR Code"/>`;
       lastInputValue = searchText.value;
       setTimeout(function () {
-        alert("QR Code gerado com sucesso!");
-      }, 1000);
+        showToast("QR Code gerado com sucesso!");
+      });
     } else {
-      alert("Você já gerou um QR Code com esse texto.");
+      showToast("Você já gerou um QR Code com esses dados.");
     }
   } else {
-    qrContainer.innerHTML = "<p>Por favor, digite algo.</p>";
+    qrContainer.innerHTML = "<p>Por favor, digite algo!</p>";
   }
 }
 
 generateBtn.addEventListener("click", qrCodeGenerator);
+shareBtn.addEventListener("click", shareQRCode);
 searchText.addEventListener("click", clearInput);
